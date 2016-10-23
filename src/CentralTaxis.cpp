d8ed1a33b43f8a44381c931f8dc674010409c62a
Ana Santos <anaezes@gmail.com>
2016-10-23 19:12:21 +0100

    CentralTaxis: implemented getters

--- src/CentralTaxis.cpp
+++ src/CentralTaxis.cpp
@@ -1,3 +1,47 @@
 #include "CentralTaxis.h"
 #include "Customer.h"
 #include "Service.h"
+
+
+CentralTaxis::CentralTaxis(string name, string customersFile, string servicesFile) :
+	customers(),
+	services()
+{
+	this->name = name;
+	this->customersFile = customersFile;
+	this->servicesFile = servicesFile;
+}
+
+vector<Customer*> CentralTaxis::getCustomers() const
+{
+	return customers;
+}
+
+
+vector<Service*> CentralTaxis::getServices() const
+{
+	return services;
+}
+
+string CentralTaxis::getCustomersFileName() const
+{
+	return customersFile;
+}
+string CentralTaxis::getServicesFileName() const
+{
+	return servicesFile;
+}
+
+//Customer* CentralTaxis::addCustomer(string name, string adress, unsigned int nif)
+//{
+//
+//}
+//Service* CentralTaxis::addService()
+//{
+//}
+//bool CentralTaxis::editCustomer()
+//{
+//}
+//bool CentralTaxis::removeCustomer()
+//{
+//}
--- src/CentralTaxis.h
+++ src/CentralTaxis.h
@@ -10,14 +10,29 @@
 #include <string>
 using namespace std;
 
-
+class Customer;
+class Service;
 
 class CentralTaxis
 {
 private:
-
+	string name;
+	string customersFile;
+	string servicesFile;
+	vector<Customer*> customers;
+	vector<Service*> services;
 public:
-CentralTaxis();
+	CentralTaxis(string name, string customersFile, string servicesFile);
+	~CentralTaxis();
+	vector<Customer*> getCustomers() const;
+	vector<Service*> getServices() const;
+	string getCustomersFileName() const;
+	string getServicesFileName() const;
+//	Customer* addCustomer(string name, string address, unsigned int nif);
+//	Service* addService();
+//	bool editCustomer();
+//	bool removeCustomer();
+
 
 };
 
