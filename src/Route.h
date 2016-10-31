825ea712c29479273fe796dff98b65159a424583
up201506197 <up201506197@fe.up.pt>
2016-10-31 22:12:11 +0000

    route class added

--- src/Route.h
+++ src/Route.h
@@ -8,7 +8,31 @@
 
 #include <vector>
 #include <string>
+
 using namespace std;
+
+class Route
+{
+private:
+	string source;
+	string arrival;
+	double distance;
+	double expectedTime;
+
+public:
+	Route(string source, string arrival, double distance, double expectedTime);
+	~Route();
+	string getSource();
+	string getArrival();
+	double getDistance();
+	double getExpectedTime();
+	void setSource(string source);
+	void setArrival(string arrival);
+	void setDistance (double distance);
+	void setExpectedTime(double expectedTime);
+
+};
+
 
 
 
