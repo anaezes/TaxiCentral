5638603d2c4847d1a3c826bc615e3faef7d44da5
Ana Santos <anaezes@gmail.com>
2016-10-31 22:39:27 +0000

    CentralTaxis: ReadCustomerFile and ReadServiceFile completed

--- src/CentralTaxis.h
+++ src/CentralTaxis.h
@@ -8,16 +8,16 @@
 
 #include "Customer.h"
 #include "Service.h"
+#include "Route.h"
+#include "Date.h"
 
 #include <vector>
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
-
-class Customer;
-class Service;
 
 class CentralTaxis
 {
@@ -29,18 +29,29 @@ private:
 	vector<Customer*> customers;
 	vector<Service*> services;
 	vector<Routes*> routes;
+
 public:
 	CentralTaxis(string name, string customersFile, string servicesFile, string routesFile);
 	~CentralTaxis();
+
 	vector<Customer*> getCustomers() const;
 	vector<Service*> getServices() const;
+	vector<Route*> getRoutes() const;
+
 	string getCustomersFileName() const;
 	string getServicesFileName() const;
-	void partitionCustomer(string line, vector<Customer> &customers);
-	bool readFileCustomers(vector<Customer> &customers, string &fileCustomers);
-	void partitionService(string line, vector<Service> &services);
-	bool readFileServices(vector<Service> &services, string &fileServices);
-//	void partitionRoute(string line, vector<Route> &routes);
-//	bool readFileRoute(vector<Route> &customers, string &fileRoutes);
+	string getRoutesFileName() const;
+
+	void addService(Service* service);
+	void addRoute(Route* route);
+	void addCustomer(Customer* customer);
+	bool editCustomer(unsigned int nif);
+	bool removeCustomer(unsigned int nif);
+
+	bool readFile(const string &fileName, vector<string> &fileLines);
+	bool readCustomersFile();
+	bool readServicesFile();
+	bool readRoutesFile();
 };
+
 #endif /* CentralTaxis_H_ */
