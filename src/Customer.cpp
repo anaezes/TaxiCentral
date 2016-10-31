fe390bd89487113bdd68451f12daacbb00afaba6
Ana Santos <anaezes@gmail.com>
2016-10-31 20:03:22 +0000

    Customer: fixed customer class

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -1,21 +1,17 @@
 #include "Customer.h"
-#include <iostream>
 
 // define class Customer
 
-Customer::Customer(unsigned int nif, string name, string address, int phoneNumber, int points)
+Customer::Customer(unsigned int nif, string name, string address, int phoneNumber)
 {
 	this->nif = nif;
 	this->name = name;
 	this->address = address;
 	this->phoneNumber = phoneNumber;
-	this->points = points;
 }
 
-Customer::~Customer()
-{
+Customer::~Customer() {}
 
-}
 string Customer::getName()
 {
 	return name;
@@ -33,11 +29,6 @@ int Customer::getPhoneNumber()
 	return phoneNumber;
 }
 
-float Customer::getPoints()
-{
-	return points;
-}
-
 unsigned int Customer::getNif() const
 {
 	return nif;
@@ -52,11 +43,6 @@ void Customer::setAddress(string address)
 {
 	this->address= address;
 }
-
-//void Customer::setPoints(float points)
-//{
-//	this->points=points;
-//}
 
 void Customer::setPhoneNumber(int phoneNumber)
 {
