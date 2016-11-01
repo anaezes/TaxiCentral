6be298cb391714883906ef3a4181eb50b98e525e
Francisca Cerquinho <franciscacerquinho@hotmail.com>
2016-11-01 14:49:07 +0000

    service class added

--- src/Service.cpp
+++ src/Service.cpp
@@ -3,7 +3,41 @@
 
 // define class Customer
 
-Service::Service()
+Service::Service(Customer* customer, double cost, Route* route, Date date, string payment)
 {
 
+	this->customer= customer;
+	this->cost=cost;
+	this->route=route;
+	this->date= date;
+	this->payment= payment;
+
 }
+
+Service::~Service(){}
+
+Customer* Service::getCustomer()
+{
+	return customer;
+}
+
+double Service::getCost()
+{
+	return cost;
+}
+
+Route Service::getRoute()
+{
+	return route;
+}
+
+Date Service::getDate()
+{
+	return date;
+}
+
+string Service::getPayment()
+ {
+ 	 return payment;
+ }
+
--- src/Service.h
+++ src/Service.h
@@ -6,18 +6,33 @@
 #ifndef SERVICE_H_
 #define SERVICE_H_
 
+#include "Route.h"
+#include "Date.h"
+
 #include <vector>
 #include <string>
+
 using namespace std;
-
-
 
 class Service
 {
 private:
+	Customer* customer;
+	double cost;
+	Route* route;
+	Date date;
+	string payment;
 
 public:
-	Service();
+	static double rateForKm;
+	static double rateForExtraMin;
+	Service(Customer* customer, double cost, Route* route, Date date, string payment);
+	~Service();
+	Customer* getCustomer();
+	double getCost();
+	Route getRoute();
+	Date getDate();
+	string getPayment();
 };
 
 
